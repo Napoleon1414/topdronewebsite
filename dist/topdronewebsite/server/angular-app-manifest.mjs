40f2d9aa947bb106aaf8097fe4d071adaf9100aa
Napoleon1414 <jraulp14@gmail.com>
2025-07-01 13:07:27 -0500

    more changes on the looks

--- dist/topdronewebsite/server/angular-app-manifest.mjs
+++ dist/topdronewebsite/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 499, hash: '5f3fc35eed3fb9bc783b08b055a02f14c19da97092324504cea8fda6bf67a449', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 1012, hash: '9996617fea3833b548e8ceab7e5f931247872645e201ef362eb0e1306ea56cf8', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 13294, hash: '3ff1adb7ce534a52d8de5a42b883d2d6f7b77b387d54545e6a59edc72797761b', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 527, hash: '9ce42a44d05f11b4459c0fafb933866ec759dbd83b869ce0fbcc8dadeae461e5', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 1040, hash: '14d169c1e43b167281ab64a2ec5ef2d8875fa4508e59842e0830fb16fe1018b1', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 13851, hash: 'd394470ca7b409c95c83a1d2f34f1bbdc8fb66095759806e2cec23fc13d39978', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
